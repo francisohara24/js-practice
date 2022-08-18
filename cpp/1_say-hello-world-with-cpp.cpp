67f9e8ba4328085a6d6b2541d8855344b18063a9
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-08-18 04:13:36 +0000

    cpp initial-commit

---
+++ cpp/1_say-hello-world-with-cpp.cpp
@@ -0,0 +1,11 @@
+//
+// Created by franc on 8/18/2022.
+//
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
---
+++ cpp/2_input-and-output.cpp
@@ -0,0 +1,16 @@
+//
+// Created by franc on 8/18/2022.
+//
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b,c;std::cin>>a>>b>>c;
+    std::cout<<a+b+c;
+    return 0;
+}
