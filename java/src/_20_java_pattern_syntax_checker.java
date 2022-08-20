e06e38b004b1ab82ef1517890ff528b469b6f8f8
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-08-20 01:31:49 +0000

    update: java-pattern-syntax-checker

--- java/src/_20_java_pattern_syntax_checker.java
+++ java/src/_20_java_pattern_syntax_checker.java
@@ -1,6 +1,19 @@
+import java.util.*;
+import java.util.regex.*;
+
 public class _20_java_pattern_syntax_checker {
-    public static void main(String[] args){
 
-
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        input.nextLine();
+        for(int i = 0; i < N; i ++) {
+            try{
+                Pattern.compile(input.nextLine());
+                System.out.println("Valid");
+            } catch(PatternSyntaxException e){
+                System.out.println("Invalid");
+            }
+        }
     }
 }
