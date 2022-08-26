8811fb4a0aad90e4b62600c5b29adab2adf31d0a
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-08-26 17:01:40 +0000

    Python: Update 2 the-minion-game - 1

--- python/2-the-minion-game.py
+++ python/2-the-minion-game.py
@@ -1,26 +1,20 @@
 # The Minion Game
-def substrings(string: str) -> set:
-    """Returns a set of all possible substrings of a given string"""
-    result = []
-    for start in range(len(string)):
-        for end in range(start, len(string)):
-            result.append(string[start:end + 1])
-    return set(result)  # remove duplicate substrings
-
 
 def minion_game(string):
-    # obtain score for stuart and kevin
-    stuart = 0
     kevin = 0
+    stuart = 0
     vowels = "AEIOU"
-    for sub_str in substrings(string):
-        main_str = string
-        while sub_str in main_str:
-            if sub_str[0] in vowels:
+    length = len(string)
+    start = 0
+    while start < length:
+        end = start
+        while end < length:
+            if string[start] in vowels:
                 kevin += 1
             else:
                 stuart += 1
-            main_str = main_str[main_str.find(sub_str) + 1:]
+            end += 1
+        start += 1
 
     # compare scores
     if kevin > stuart:
@@ -31,4 +25,4 @@ def minion_game(string):
         print("Draw")
 
 
-minion_game("BANANA")
+minion_game(input())
