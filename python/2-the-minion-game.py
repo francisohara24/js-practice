b7474cef7e862b586dd288ea62b8224b69f3e156
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-09-02 12:35:10 +0000

    Python: Final update 2-the-minion-game

--- python/2-the-minion-game.py
+++ python/2-the-minion-game.py
@@ -1,20 +1,12 @@
-# The Minion Game
-
 def minion_game(string):
     kevin = 0
     stuart = 0
     vowels = "AEIOU"
-    length = len(string)
-    start = 0
-    while start < length:
-        end = start
-        while end < length:
-            if string[start] in vowels:
-                kevin += 1
-            else:
-                stuart += 1
-            end += 1
-        start += 1
+    for i in range(len(string)):
+        if string[i] in vowels:
+            kevin += len(string) - i
+        else:
+            stuart += len(string) - i
 
     # compare scores
     if kevin > stuart:
@@ -25,4 +17,6 @@ def minion_game(string):
         print("Draw")
 
 
-minion_game(input())
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
