98c1699e8f6b64444ffc70997e3b9aa3f4dc9894
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-09-06 17:29:46 +0000

    algorithms: final-update 10-anagram

--- algorithms-py/10-anagram.py
+++ algorithms-py/10-anagram.py
@@ -1,14 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+#
+# Complete the 'anagram' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
 
 def anagram(s):
-    result = 0
-    previous = ""
-    first = s[:len(s)//2]
-    second = s[len(s)//2:]
-    if len(s) % 2 == 1: return -1
-    for char in first:
-        if char not in previous:
-            result += abs(first.count(char) - second.count(char))
-            previous = previous + char
-    return result
+    if len(s) % 2 != 0:
+        return -1
+    s1 = s[:len(s) // 2]
+    s2 = s[len(s) // 2:]
+    n_changes = len(s1)
+    for char in set([i for i in s1]):
+        n_changes -= min(s1.count(char), s2.count(char))
+    return n_changes
 
-print(anagram("fdhlvosfpafhalll"))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = anagram(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
