97b7bd9c75cfdf3845705778f4a0051b059aaec8
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-09-24 20:31:13 +0000

    Add _1_InsertionSortPart1s

--- algorithms-java/src/_1_InsertionSortPart1.java
+++ algorithms-java/src/_1_InsertionSortPart1.java
@@ -1,4 +1,21 @@
+import java.util.List;
 
-public class _1_InsertionSortPart1 {
+public class _1_InsertionSortPart1{
+
+    public static void insertionSort1(int n, List<Integer> arr){
+        int target = arr.get(arr.size()-1);
+        for (int i = arr.size() - 1; i >= 0; i --){
+            if (arr.get(i) <= target) {
+                arr.set(i, target);
+                break;
+            }
+            else {
+                arr.set(i, arr.get(i-1))
+            }
+        }
+    }
+
+
+
 
 }
