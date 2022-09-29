4793d20dd91f9bd44f3549bc81559078e2ca5bc3
francis-ohara <francis-ohara@outlook.com>
2022-09-29 10:59:49 +0000

    Update _2_InsertionSortPart2

--- algorithms-java/src/_2_InsertionSortPart2.java
+++ algorithms-java/src/_2_InsertionSortPart2.java
@@ -1,6 +1,25 @@
-import java.util.List;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class _2_InsertionSortPart2 {
+class Result {
+
+    /*
+     * Complete the 'insertionSort2' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY arr
+     */
+
     public static void arrPrint(List<Integer> arr){
         for (int value: arr) System.out.print(value + " ");
     }
@@ -14,15 +33,33 @@ public class _2_InsertionSortPart2 {
                     arrPrint(arr);
                     System.out.println();
                 }
-                else if (arr.get(j-1) < target) {
+                else if (arr.get(j-1) < target){
                     arr.set(j, target);
                     arrPrint(arr);
                     System.out.println();
+                    break;
                 }
                 else
                     arr.set(j, arr.get(j-1));
 
             }
         }
+    }
+
+}
+
+public class _2_InsertionSortPart2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.insertionSort2(n, arr);
+
+        bufferedReader.close();
     }
 }
