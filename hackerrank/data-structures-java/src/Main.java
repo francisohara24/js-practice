3781b659d43ece359709f92f53a18dd1a7ef9b6d
Francis O'Hara Aidoo <francis-ohara@outlook.com>
2022-10-16 22:27:51 +0000

    data-structures: add 1-2D-Arrays

---
+++ hackerrank/data-structures-java/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Main{
+
+    public static void main(String[] args){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(12);
+        System.out.println(numbers.get(0));
+
+    }
+}
---
+++ hackerrank/data-structures-java/src/_1_2D_Array_DS.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'hourglassSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int hourglassSum(List<List<Integer>> arr) {
+        ArrayList<Integer> sums = new ArrayList<>();
+
+        for(int i = 0; i < 4; i++)
+            for(int j = 0; j < 4; j++)
+                sums.add(arr.get(i).get(j) + arr.get(i).get(j+1) + arr.get(i).get(j+2) + arr.get(i+1).get(j+1) + arr.get(i+2).get(j) + arr.get(i+2).get(j+1) + arr.get(i+2).get(j+2));
+
+
+        int result = sums.get(2);
+        for (int sum: sums)
+            if (sum > result)
+                result = sum;
+        return result;
+
+
+    }
+
+}
+
+public class _1_2D_Array_DS {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, 6).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.hourglassSum(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
