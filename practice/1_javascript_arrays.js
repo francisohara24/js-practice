a7f092826e4bf0ac3deee2e398fc8e0d9db02d46
francisohara24 <franciskohara@gmail.com>
2025-04-12 05:17:08 -0400

    feat: add program for demonstrating array syntax in JavaScript

---
+++ practice/1_javascript_arrays.js
@@ -0,0 +1,9 @@
+/**
+ * @file   1_javascript_arrays.js
+ * @brief  A program to demonstrate syntax for creating and using arrays in JavaScript.
+ * @author Francis O'Hara
+ * @date   4/12/25
+ */
+let arr = [1, 2.0, "three", true];
+
+let arr2 = []
