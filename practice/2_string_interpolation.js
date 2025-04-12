6727bb12c56156dbbcbedd390784c9ef62f1eaf8
francisohara24 <franciskohara@gmail.com>
2025-04-12 04:41:10 -0400

    feat: add code to demonstrate string interpolation in JavaScript

---
+++ practice/2_string_interpolation.js
@@ -0,0 +1,14 @@
+let meaning_of_life = 42;
+let string = `${meaning_of_life} is the meaning of Life, the Universe, and Everything.`;
+console.log(string);
+
+let animals = ["Birds", "Dogs", "Cats", "Lizards"]
+console.log(`${animals} are examples of animals in the real world`);
+
+/**
+ * CONCLUSIONS
+ *  1. String interpolation allows embedding anything that evaluates to a value's string representation into an existing string
+ *     in a concise manner (i.e. cleaner alternative to string concatenation).
+ *  2. The string must be enclosed in backticks ` instead of the usual quotes "" or ''.
+ *  3. The value to be interpolated must be enclosed in curly brackets prepended with a dollar sign ${}.
+ */
